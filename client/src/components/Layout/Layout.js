0064fba24dc9be5a5ad17c530ba3570a46ecfcf5
AshrayG <ashray.gupta2002@gmail.com>
2023-07-03 05:01:07 +0530

    third commit

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React, { Children } from 'react'
+import Header from './header'
+import Footer from './footer'
+
+
+const Layout = ({children}) => {
+  return (
+    <>
+      <Header/>
+      <div className='content'>
+        {children}
+      </div>
+      <Footer/>
+    </>
+  )
+}
+
+export default Layout
