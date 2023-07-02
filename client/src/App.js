bc2df032d3c6a76bae92058e719e49ab341d0109
AshrayG <ashray.gupta2002@gmail.com>
2023-07-03 05:09:36 +0530

    fourth commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+
 function App() {
   return (
-    <div className="container-fluid">
-      <h1 class= "text-center">Hello</h1>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      </Routes>
+      </>
   );
 }
 
