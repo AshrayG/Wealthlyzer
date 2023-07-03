bc63654b867f5069bb637d3ceb351a0eeff481be
AshrayG <ashray.gupta2002@gmail.com>
2023-07-04 02:43:01 +0530

    protected login register

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import {Form,Input,message} from 'antd'
 import { Link,useNavigate } from 'react-router-dom'
 import axios from 'axios'
@@ -22,7 +22,12 @@ const Login = () => {
             message.error('invalid username or password')
         }
 
-    }
+    };
+    useEffect(()=> {
+        if(localStorage.getItem("user")){
+            navigate('/')
+        }
+      },[navigate])
   return (
     <>
     <div className='login-page'>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import {Form,Input,message} from 'antd'
 import { Link,useNavigate} from 'react-router-dom'
 import axios from 'axios'
@@ -20,7 +20,11 @@ const Register = () => {
             message.error("something went wrong");
       }
     }
-  
+  useEffect(()=> {
+    if(localStorage.getItem("user")){
+        navigate('/')
+    }
+  },[navigate])
   return (
     <>
       <div className='register-page'>
