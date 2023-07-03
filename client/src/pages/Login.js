17246d17f7453c31e42e7934856466445698a65e
AshrayG <ashray.gupta2002@gmail.com>
2023-07-03 22:47:15 +0530

    register page update

---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Form,Input} from 'antd'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Login
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import {Form,Input} from 'antd'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
+    const submitHandler = (values) => {
+        console.log(values)
+    }
   return (
     <>
       <div className='register-page'>
-        <h1>Register Page</h1>
+        <Form layout='vertical' onFinish={submitHandler}>
+            <h1>Registeration Form</h1>
+            <Form.Item label='Name' name='name'>
+                <Input/>
+            </Form.Item>            
+            <Form.Item label='Email' name='email'>
+                <Input type='email'/>
+            </Form.Item>            
+            <Form.Item label='Password' name='password'>
+                <Input type='password'/>
+            </Form.Item>
+            <div className='d-flex'>
+                <p>Already Registered? Click <Link to='/login'>Here </Link> to login </p>
+            </div>
+            <button className='btn btn-primary'>Register</button>
+        </Form>
       </div>
     </>
   )
