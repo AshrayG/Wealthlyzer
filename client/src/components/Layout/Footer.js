0b0288d61ec3b3fd931fb1a3246470fca0df3075
AshrayG <ashray.gupta2002@gmail.com>
2023-07-03 18:58:19 +0530

    third commit

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div className='bg-dark text-light'>
+    <div className='bg-dark text-light p-5'>
       <h6 className='text-center'>All rights reserved &copy; Wealthlyzer</h6>
     </div>
   )
