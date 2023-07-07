d8f20633592cbd43835695c44ebc456638578a92
AshrayG <ashray.gupta2002@gmail.com>
2023-07-07 16:44:36 +0530

    login, register,signup

---
+++ modles/transactionModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const transactionSchema = new mongoose.Schema({
+    amount:{
+        type:Number,
+        required:[true, 'amount is required']
+    },
+    category:{
+        type:String,
+        required:[true,'category is required']
+    },
+    reference:{
+        type:String,
+    },
+    description:{
+        type:String,
+        required:[true,'description is required']
+    },
+    date:{
+        type:String,
+        required:[true,'time is required']
+    },
+    timestamp:true,
+
+})
